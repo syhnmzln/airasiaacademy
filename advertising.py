f0a72b8b7fd6c8844ce538492ec4d7f13283d956
syhnmzln <113568212+syhnmzln@users.noreply.github.com>
2023-03-12 15:55:02 +0800

    Update advertising.py

--- advertising.py
+++ advertising.py
@@ -12,9 +12,9 @@ This app predicts the **Sales forecasting** value!
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    TV = st.sidebar.slider('TV', 0.7, 296.4, 149.7)
-    Radio = st.sidebar.slider('Radio', 0.0, 49.6, 22.9)
-    Newspaper = st.sidebar.slider('Newspaper', 0.3, 114.0, 25.7)
+    TV = st.sidebar.slider('TV', 0.0, 500, 250 )
+    Radio = st.sidebar.slider('Radio', 0.0, 150, 75)
+    Newspaper = st.sidebar.slider('Newspaper', 0.0, 200, 100)
     data = {'TV': TV,
             'Radio': Radio,
             'Newspaper': Newspaper}
