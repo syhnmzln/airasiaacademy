f4c955f379edb7dae7156d8f7ec17c17a5bc035c
syhnmzln <113568212+syhnmzln@users.noreply.github.com>
2023-03-12 15:07:16 +0800

    Update advertising.py

--- advertising.py
+++ advertising.py
@@ -31,5 +31,4 @@ loaded_model = pickle.load(open("AdvertisingSVM.h5", "rb"))
 prediction = loaded_model.predict(df)
 
 st.subheader('Prediction')
-data = {'Sales predicted': Sales}
-st.write(prediction)
+st.write(Predicted)
