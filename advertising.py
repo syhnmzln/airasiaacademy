ab3a80ceb3b0b06c3d0c5afdd03382e6a3de67bb
syhnmzln <113568212+syhnmzln@users.noreply.github.com>
2023-03-12 14:41:22 +0800

    Add files via upload

---
+++ advertising.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+st.write("""
+# Advertising Sales Forecasting App
+
+This app predicts the **Sales forecasting** value!
+""")
+
+st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+    TV = st.sidebar.slider('TV', 0.7, 296.4, 149.7)
+    Radio = st.sidebar.slider('Radio', 0.0, 49.6, 22.9)
+    Newspaper = st.sidebar.slider('Newspaper', 0.3, 114.0, 25.7)
+    data = {'TV': TV,
+            'Radio': Radio,
+            'petal_length': petal_length,
+            'Newspaper': Newspaper}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df)
+
+loaded_model = pickle.load(open("AdvertisingSVM.h5", "rb"))
+
+prediction = loaded_model.predict(df)
+
+st.subheader('Prediction')
+st.write(prediction)
