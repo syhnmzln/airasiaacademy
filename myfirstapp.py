e014308b6dd432d5bc119d286b8f9fe5dfbb2048
syhnmzln <113568212+syhnmzln@users.noreply.github.com>
2023-03-12 12:00:53 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import time
 
 st.header("My first Streamlit App")
 
@@ -30,3 +31,17 @@ else:
         'Intplan': ['yes', 'yes', 'yes', 'no'],
         'Churn Status': [0, 0, 0, 1]
         }))
+        
+else:
+    'Starting a long computation...'
+    
+    latest_iteration = st.empty()
+    bar = st.progress(0)
+
+    for i in range(100):
+   
+        latest_iteration.text(f'Iteration {i+1}')
+        bar.progress(i + 1)
+        time.sleep(0.1)
+
+    '...and now we\'re done!'
